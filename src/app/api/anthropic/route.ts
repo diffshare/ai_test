3831cec7c3122e8f519fcfff6b485a1b451a07ab
diffshare <diffshare@gmail.com>
2024-03-19 09:00:56 +0000

    APIキーの読み込み箇所を変更

--- src/app/api/anthropic/route.ts
+++ src/app/api/anthropic/route.ts
@@ -1,13 +1,12 @@
 import Anthropic from "@anthropic-ai/sdk";
 import { AnthropicStream, StreamingTextResponse } from "ai";
 import { NextRequest } from "next/server";
-
-const anthropic = new Anthropic({
-    apiKey: process.env.ANTHROPIC_API_KEY
-});
   
 export async function POST(request: NextRequest) {
     try {
+        const anthropic = new Anthropic({
+            apiKey: process.env.ANTHROPIC_API_KEY
+        });        
         const json = await request.json();
         const response = anthropic.messages.stream(json)
         const stream = AnthropicStream(response);
